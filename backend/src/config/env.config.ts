bb0a75c04c5d9454ef917f48bfaefd02cfee3e79
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-12-07 20:22:11 +0530

    feat(architecture)
    backend files setup

---
+++ backend/src/config/env.config.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+  APP: {
+    PORT: process.env.APP_PORT || 3000,
+    HOST: process.env.APP_HOST || "localhost",
+    ENV: process.env.NODE_ENV || "development",
+    CLIENT_URL: process.env.CLIENT_URL || "http://localhost:3000",
+  },
+  DB: {
+    HOST: process.env.DB_HOST || "localhost",
+    PORT: Number(process.env.DB_PORT) || 5432,
+    USER: process.env.DB_USER || "user",
+    PASSWORD: process.env.DB_PASSWORD || "password",
+    NAME: process.env.DB_NAME || "database",
+  },
+  CLOUDINAIRY: {
+    CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "",
+    API_KEY: process.env.CLOUDINARY_API_KEY || "",
+    API_SECRET: process.env.CLOUDINARY_API_SECRET || "",
+  },
+  JWT: {
+    SECRET: process.env.JWT_SECRET || "your_jwt_secret",
+    EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
+    REFRESH_TOKEN_EXPIRES_IN: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN || "7d",
+  },
+  NODE_MAILER: {
+    HOST: process.env.NODE_MAILER_HOST || "smtp.example.com",
+    PORT: Number(process.env.NODE_MAILER_PORT) || 587,
+    USER: process.env.NODE_MAILER_USER || "",
+    PASSWORD: process.env.NODE_MAILER_PASSWORD || "",
+  },
+} as const;
