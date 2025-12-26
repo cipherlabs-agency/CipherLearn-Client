8c15a4ec42ec6eb4b244285fd6ce4301b1f7f9b3
Abhishek Sharma <abhishek.webdev001@gmail.com>
2025-12-26 10:48:04 +0530

    feat(analytics)
    add routes to analytics functions

--- backend/src/modules/dashboard/analytics/route.ts
+++ backend/src/modules/dashboard/analytics/route.ts
@@ -1,5 +1,25 @@
 import { Router } from "express";
+import { isAdmin, isAuthenticated } from "../../auth/middleware";
+import AnalyticsController from "./controller";
 
 const router = Router();
+router.use(isAuthenticated, isAdmin);
+
+const controller = new AnalyticsController();
+
+router.get("/batch-count", controller.getTotalBatchesCount.bind(controller));
+router.get("/student-count", controller.getTotalStudentsCount.bind(controller));
+router.get(
+  "/batch/:batchId/student-count",
+  controller.getTotalStudentsCountOfBatch.bind(controller)
+);
+router.get(
+  "/batch/:batchId/attendance/day",
+  controller.getAttendanceMatrixOfDay.bind(controller)
+);
+router.get(
+  "/batch/:batchId/attendance/month",
+  controller.getAttendanceMatrixOfMonth.bind(controller)
+);
 
 export default router;
